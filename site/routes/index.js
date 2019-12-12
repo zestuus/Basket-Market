9d237002a16426b4d8cff3cd919b998072c359f2
zestuus <a.kushka.30@gmail.com>
2019-12-13 01:58:56 +0200

    minor fixes

--- site/routes/index.js
+++ site/routes/index.js
@@ -78,6 +78,7 @@ router.get('/', checkToken, function(req, res) {
                             prev_id = products[j].id
                         }
                     }
+
                     res.render('shop/index', {
                         title: 'Basket Market',
                         logined: req.user ? true : false,
@@ -116,42 +117,53 @@ router.get('/cat/:id', checkToken, function(req, res) {
                 if (error) {
                     console.log(error.message);
                 } else {
-                    var j = 0;
-                    products = [{
-                        id: prod_res.rows[0].id,
-                        name: prod_res.rows[0].name,
-                        price: prod_res.rows[0].price,
-                        weight: prod_res.rows[0].weight,
-                        image: prod_res.rows[0].image,
-                        category: prod_res.rows[0].category,
-                        ingredients: [prod_res.rows[0].ingredient]
-                    }];
-                    var prev_id = products[j].id;
-                    for (var i = 0; i < prod_res.rows.length; i++) {
-                        if (prev_id == prod_res.rows[i].id) {
-                            products[j].ingredients.push(prod_res.rows[i].ingredient);
-                        } else {
-                            j++;
-                            products.push({
-                                id: prod_res.rows[i].id,
-                                name: prod_res.rows[i].name,
-                                price: prod_res.rows[i].price,
-                                weight: prod_res.rows[i].weight,
-                                image: prod_res.rows[i].image,
-                                category: prod_res.rows[i].category,
-                                ingredients: [prod_res.rows[i].ingredient]
-                            });
-                            prev_id = products[j].id
+                    if(prod_res.rows.length > 0){
+                        var j = 0;
+                        products = [{
+                            id: prod_res.rows[0].id,
+                            name: prod_res.rows[0].name,
+                            price: prod_res.rows[0].price,
+                            weight: prod_res.rows[0].weight,
+                            image: prod_res.rows[0].image,
+                            category: prod_res.rows[0].category,
+                            ingredients: [prod_res.rows[0].ingredient]
+                        }];
+                        var prev_id = products[j].id;
+                        for (var i = 0; i < prod_res.rows.length; i++) {
+                            if (prev_id == prod_res.rows[i].id) {
+                                products[j].ingredients.push(prod_res.rows[i].ingredient);
+                            } else {
+                                j++;
+                                products.push({
+                                    id: prod_res.rows[i].id,
+                                    name: prod_res.rows[i].name,
+                                    price: prod_res.rows[i].price,
+                                    weight: prod_res.rows[i].weight,
+                                    image: prod_res.rows[i].image,
+                                    category: prod_res.rows[i].category,
+                                    ingredients: [prod_res.rows[i].ingredient]
+                                });
+                                prev_id = products[j].id
+                            }
                         }
+                        res.render('shop/index', {
+                            title: 'Basket Market',
+                            logined: req.user ? true : false,
+                            isAdmin: req.user && req.user.email == "admin@basket-market.com",
+                            questions: [],
+                            categories: cat_res.rows,
+                            products: products
+                        });
+                    } else {
+                        res.render('shop/index', {
+                            title: 'Basket Market',
+                            logined: req.user ? true : false,
+                            isAdmin: req.user && req.user.email == "admin@basket-market.com",
+                            questions: [],
+                            categories: cat_res.rows,
+                            products: []
+                        });
                     }
-                    res.render('shop/index', {
-                        title: 'Basket Market',
-                        logined: req.user ? true : false,
-                        isAdmin: req.user && req.user.email == "admin@basket-market.com",
-                        questions: [],
-                        categories: cat_res.rows,
-                        products: products
-                    });
                 }
             });
         }
